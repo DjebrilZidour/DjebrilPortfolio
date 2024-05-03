adace50178726d89cffe9f9e905a239d8773d57f
Djebril <djebrilacademy@gmail.com>
2024-05-03 21:12:39 +0100

    add lib

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar"
+
+const Blog = ()=>{
+    return(
+        <>
+        <Navbar/>
+        <section>
+            <h1 className="text-5xl">blog</h1>
+        </section>
+        </>
+    )
+}
+export default Blog
