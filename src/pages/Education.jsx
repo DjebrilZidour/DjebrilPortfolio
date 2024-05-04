841d0def8e9fdf2b2fc4c06f7938aea9a91203f2
Djebril <djebrilacademy@gmail.com>
2024-05-04 20:31:56 +0100

    good upgrade add some responsive background

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,32 +1,75 @@
 const Education = () => {
   return (
     <>
-      <h1 className="text-3xl">education</h1>
       <section className="flex justify-center items-center gap-4 flex-col mx-24">
+        <h1 className="text-3xl">education</h1>
+
         <div className="flex justify-center items-center gap-8 rounded-3xl border-2 border-black">
           <div>
-            <div className="w-full bg-red-300 flex justify-center items-center rounded-3xl">
-                <h1 className="text-4xl">LYCEE</h1>
+            <div className="w-full bg-red-300 flex justify-center items-center rounded-3xl py-4">
+              <h1 className="text-4xl">High School</h1>
             </div>
 
-            <div className="flex justify-center items-center gap-4 flex-col w-1/2 py-4 px-8">
-              <p>
+            <div className="flex justify-center items-center gap-8  py-4 px-8">
+              <p className="text-xl">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
                 ad tenetur quisquam porro adipisci hic harum, quam distinctio
                 enim voluptatem minima doloribus praesentium quasi, culpa
                 suscipit minus ex ullam iure. Itaque, obcaecati vel fugiat
                 deserunt provident error impedit ipsa atque harum earum eius
                 nobis quas quia fugit suscipit, voluptatem, non doloribus
-                voluptatibus. Vero obcaecati minima, iure dolor nihil ullam est!
-                Officia inventore a iusto blanditiis amet velit ab, perferendis
-                repellendus explicabo corporis hic nobis neque aspernatur
-                sapiente. Iusto quibusdam omnis atque consequatur soluta saepe,
-                doloremque corporis dolore animi officiis rerum!
               </p>
+              <img
+                className="rounded-3xl"
+                src="https://lh5.googleusercontent.com/p/AF1QipNjw1uVRhT0wBVp95edtgeYloI-aNK3JdyLn_cs=w427-h240-k-no"
+                alt="schoolPicture"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center gap-8 rounded-3xl border-2 border-black">
+          <div>
+            <div className="w-full bg-red-300 flex justify-center items-center rounded-3xl py-4">
+              <h1 className="text-4xl">High School</h1>
             </div>
 
-            <div className="w-1/2">
-              <img src="#" alt="schoolPicture" />
+            <div className="flex justify-center items-center gap-8  py-4 px-8">
+              <p className="text-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+                ad tenetur quisquam porro adipisci hic harum, quam distinctio
+                enim voluptatem minima doloribus praesentium quasi, culpa
+                suscipit minus ex ullam iure. Itaque, obcaecati vel fugiat
+                deserunt provident error impedit ipsa atque harum earum eius
+                nobis quas quia fugit suscipit, voluptatem, non doloribus
+              </p>
+              <img
+                className="rounded-3xl"
+                src="https://lh5.googleusercontent.com/p/AF1QipNjw1uVRhT0wBVp95edtgeYloI-aNK3JdyLn_cs=w427-h240-k-no"
+                alt="schoolPicture"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center gap-8 rounded-3xl border-2 border-black">
+          <div>
+            <div className="w-full bg-red-300 flex justify-center items-center rounded-3xl py-4">
+              <h1 className="text-4xl">High School</h1>
+            </div>
+
+            <div className="flex justify-center items-center gap-8  py-4 px-8">
+              <p className="text-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+                ad tenetur quisquam porro adipisci hic harum, quam distinctio
+                enim voluptatem minima doloribus praesentium quasi, culpa
+                suscipit minus ex ullam iure. Itaque, obcaecati vel fugiat
+                deserunt provident error impedit ipsa atque harum earum eius
+                nobis quas quia fugit suscipit, voluptatem, non doloribus
+              </p>
+              <img
+                className="rounded-3xl"
+                src="https://lh5.googleusercontent.com/p/AF1QipNjw1uVRhT0wBVp95edtgeYloI-aNK3JdyLn_cs=w427-h240-k-no"
+                alt="schoolPicture"
+              />
             </div>
           </div>
         </div>
--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -1,10 +1,16 @@
 const Skill =()=>{
     return(
-        <section className="flex justify-center items-center gap-4 mx-12 my-12 flex-col">
+        <>
+        <div>
        
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-4xl">my skills</h1>
-          <p>
+        </div>
+
+       
+        <section className="flex justify-center items-center gap-4 mx-12 my-12 gap-32">
+       
+        <div className="flex flex-col justify-center items-start gap-4">
+        <h1 className="text-5xl">My skills</h1>
+          <p className="w-96 text-2xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quis magni
             temporibus recusandae dolorum dolore iusto, vitae aperiam, quaerat
             aliquam autem incidunt esse nobis enim voluptate, beatae praesentium
@@ -17,20 +23,22 @@ const Skill =()=>{
             <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/5968/5968267.png" alt="logo" />
             <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/5968/5968242.png" alt="logo" />
             <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/5968/5968292.png" alt="logo" />
-            <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/1126/1126012.png" alt="logo" />
-            <img className="w-12" src="https://imgs.search.brave.com/p5MjVdZG2puGdL4uizeF2dUQHiuwXKEw6-BPZ3zlGJE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi9kL2RiL05w/bS1sb2dvLnN2Zy8y/MjBweC1OcG0tbG9n/by5zdmcucG5n" alt="logo" />
-            <img className="w-12" src="#" alt="logo" />
+           
+            
           </div>
           <div className="flex justify-center items-center gap-8">
-            <img className="w-12" src="#" alt="logo" />
-            <img className="w-12" src="#" alt="logo" />
-            <img className="w-12" src="#" alt="logo" />
-            <img className="w-12" src="#" alt="logo" />
-            <img className="w-12" src="#" alt="logo" />
-            <img className="w-12" src="#" alt="logo" />
+          <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/1126/1126012.png" alt="logo" />
+            <img className="w-12" src="https://imgs.search.brave.com/p5MjVdZG2puGdL4uizeF2dUQHiuwXKEw6-BPZ3zlGJE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi9kL2RiL05w/bS1sb2dvLnN2Zy8y/MjBweC1OcG0tbG9n/by5zdmcucG5n" alt="logo" />
+            <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png" alt="logo" />
+          </div>
+          <div className="flex justify-center items-center gap-8">
+          <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/15466/15466163.png" alt="logo" />
+            <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/16021/16021236.png" alt="logo" />
+            <img className="w-12" src="https://imgs.search.brave.com/EKsk4xtJu-qcCLu6v_GJEiJbWXakB9PzJp_snmXg4ak/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy90YWlsd2luZC1j/c3MtMi5zdmc.svg" alt="logo" />
           </div>
         </div>
         </section>
+        </>
     )
 }
 export default Skill
