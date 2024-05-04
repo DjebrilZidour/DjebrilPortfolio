b6a674fbf4962fd05dcf3e7f929991fc4652b133
Djebril <djebrilacademy@gmail.com>
2024-05-04 20:54:08 +0100

    fix responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,13 @@ import { useNavigate } from "react-router";
 
 import { TypeAnimation } from "react-type-animation";
 const Home = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   return (
     <>
-      <section className="flex justify-between items-start lg:item-center my-8 home px-12 ">
-        <div className="flex flex-col items-center lg:items-start content-start justify-start lg:w-1/2 lg:gap-4 gap-12">
-          <h1 className="text-5xl">
+      <section className="flex lg:justify-between lg:items-center items-start  my-8 home px-12  ">
+        
+        <div className="flex flex-col lg:items-start lg:items-start lg:justify-center lg:w-1/2 lg:gap-4 gap-12 mt-24">
+          <h1 className="text-5xl ">
             My Name is{" "}
             <TypeAnimation
               sequence={[
@@ -31,7 +32,6 @@ const Home = () => {
               repeat={Infinity}
             />
           </h1>
-
           <p className="text-3xl">
             an Algerian student living in algeirs , I'm 16 years old i have
             about 4 years in the field of web development actually I have some
@@ -42,24 +42,25 @@ const Home = () => {
             and biking well you can see my skills on development by scrolling on
             this portfolio
           </p>
-
           <div className="flex justify-center items-center gap-4">
-            <a target="new" href="https://github.com/DjebrilZidour">
-              <div className="border px-4 py-2 bg-white lg:bg-none hover:bg-red-500 hover:text-white rounded-xl text-2xl capitalize uppercase flex justify-center items-center gap-4 cursor-poiter">
+          <a target="new" href="https://github.com/DjebrilZidour">
+              <div  className="border px-4 py-2 hover:bg-red-500 hover:text-white bg-white rounded-xl text-2xl capitalize uppercase flex justify-center items-center gap-4 cursor-poiter">
+                
                 <p className="cursor-pointer">my Github </p>
                 <img
                   className="w-8 h-8"
                   src="https://cdn-icons-png.flaticon.com/128/1051/1051326.png"
                 />
+              
+               
               </div>
-            </a>
-
-            <button className="border px-4 py-2 hover:bg-red-500 hover:text-white rounded-xl text-2xl capitalize uppercase bg-white ">
+              </a>
+           
+            <button className="border px-4 py-2 bg-white hover:bg-red-500 hover:text-white rounded-xl text-2xl capitalize uppercase">
               {" "}
               Hire Me
             </button>
           </div>
-
         </div>
       </section>
     </>
