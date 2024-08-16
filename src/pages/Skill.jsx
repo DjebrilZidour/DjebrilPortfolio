c74c2c29bbc513eb7ce61692ffdc7806518d9426
Djebril <djebrilacademy@gmail.com>
2024-08-16 11:06:21 +0100

    adding a very beutiful style

--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -3,7 +3,7 @@ const Skill =()=>{
         <>
       
        
-        <section className="flex  flex-col md:flex-row items-center justify-center md:gap-32 gap-8">
+        <section className="flex  flex-col md:flex-row items-center justify-center md:gap-32 gap-8 bg-yellow-100 py-12">
        
         <div className="flex flex-col justify-center items-center md:items-start gap-4">
         <h1 className="text-5xl lg:text-start">My skills</h1>
@@ -17,21 +17,21 @@ const Skill =()=>{
         
         <div className="flex justify-center items-center gap-4 flex-col">
           <div className="flex justify-center items-center gap-8">
-            <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/5968/5968267.png" alt="logo" />
-            <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/5968/5968242.png" alt="logo" />
-            <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/5968/5968292.png" alt="logo" />
+            <img className="md:w-12 w-20" src="https://cdn-icons-png.flaticon.com/128/5968/5968267.png" alt="logo" />
+            <img className="md:w-12 w-20" src="https://cdn-icons-png.flaticon.com/128/5968/5968242.png" alt="logo" />
+            <img className="md:w-12 w-20" src="https://cdn-icons-png.flaticon.com/128/5968/5968292.png" alt="logo" />
            
             
           </div>
           <div className="flex justify-center items-center gap-8">
-          <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/1126/1126012.png" alt="logo" />
-            <img className="w-12" src="https://imgs.search.brave.com/p5MjVdZG2puGdL4uizeF2dUQHiuwXKEw6-BPZ3zlGJE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi9kL2RiL05w/bS1sb2dvLnN2Zy8y/MjBweC1OcG0tbG9n/by5zdmcucG5n" alt="logo" />
-            <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png" alt="logo" />
+          <img className="md:w-12 w-20" src="https://cdn-icons-png.flaticon.com/128/1126/1126012.png" alt="logo" />
+            <img className="md:w-12 w-20" src="https://imgs.search.brave.com/p5MjVdZG2puGdL4uizeF2dUQHiuwXKEw6-BPZ3zlGJE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi9kL2RiL05w/bS1sb2dvLnN2Zy8y/MjBweC1OcG0tbG9n/by5zdmcucG5n" alt="logo" />
+            <img className="md:w-12 w-20" src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png" alt="logo" />
           </div>
           <div className="flex justify-center items-center gap-8">
-          <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/15466/15466163.png" alt="logo" />
-            <img className="w-12" src="https://cdn-icons-png.flaticon.com/128/16021/16021236.png" alt="logo" />
-            <img className="w-12" src="https://imgs.search.brave.com/EKsk4xtJu-qcCLu6v_GJEiJbWXakB9PzJp_snmXg4ak/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy90YWlsd2luZC1j/c3MtMi5zdmc.svg" alt="logo" />
+          <img className="md:w-12 w-20" src="https://cdn-icons-png.flaticon.com/128/15466/15466163.png" alt="logo" />
+            <img className="md:w-12 w-20" src="https://cdn-icons-png.flaticon.com/128/16021/16021236.png" alt="logo" />
+            <img className="md:w-12 w-20" src="https://imgs.search.brave.com/EKsk4xtJu-qcCLu6v_GJEiJbWXakB9PzJp_snmXg4ak/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jZG4u/d29ybGR2ZWN0b3Js/b2dvLmNvbS9sb2dv/cy90YWlsd2luZC1j/c3MtMi5zdmc.svg" alt="logo" />
           </div>
         </div>
         </section>
