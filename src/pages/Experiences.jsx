7616f1c7271763c1023fc999f29ffe9ce898a226
Djebril <djebrilacademy@gmail.com>
2024-09-03 20:26:35 +0200

    adding description for the skills

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -1,11 +1,32 @@
 const Experiences = () => {
   return (
     <section id="experience">
-      <h1>experiances</h1>
-      <div> dd</div>
-      <div></div>
-      <div></div>
-    </section> 
+      <h1>Diplomes</h1>
+      <div className="flex justify-center items-center flex-col md:flex-row">
+        <div className="flex flex-col justify-center items-center ">
+          <img src="" alt="" />
+          <h1 className="capitalize md:text-3xl text-2xl">Front end web dev</h1>
+          <p></p>
+        </div>
+        <div className="flex flex-col justify-center items-center ">
+          <img src="" alt="" />
+          <h1 className="capitalize md:text-3xl text-2xl">Global marketing code 213</h1>
+          <p></p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center flex-col md:flex-row">
+        <div className="flex flex-col justify-center items-center ">
+          <img src="" alt="" />
+          <h1 className="capitalize md:text-3xl text-2xl">Facebook Ads izi Road</h1>
+          <p></p>
+        </div>
+        <div className="flex flex-col justify-center items-center ">
+          <img src="" alt="" />
+          <h1 className="capitalize md:text-3xl text-2xl">robotic and arduino at Techmology</h1>
+          <p></p>
+        </div>
+      </div>
+    </section>
   );
 };
 export default Experiences;
--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -6,81 +6,104 @@ const Skill = () => {
         className="flex  flex-col gap-8 gap-8 bg-yellow-100 py-12"
       >
         <div className="flex justify-center items-center gap-8">
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/9375/9375367.png"
-              alt=""
-              className="w-24"
-            />
-            <h1 className="text-5xl lg:text-start">My skills</h1>
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/9375/9375367.png"
+            alt=""
+            className="md:w-24 w-16"
+          />
+          <h1 className="text-5xl lg:text-start">My skills</h1>
+        </div>
+        <div className="flex  items-center justify-center md:flex-row flex-col gap-8">
+          <div className="flex flex-col justify-center items-center md:items-start gap-4 md:w-1/2 w-full md:p-0 px-6">
+            <div className="flex justify-center items-start gap-4 ">
+              <img
+                className="w-8"
+                src="https://cdn-icons-png.flaticon.com/128/10093/10093570.png"
+                alt=""
+              />
+              <p className="text-3xl md:text-start text-center "> Create dynamic and engaging front-end user interfaces for your web application</p>
+            </div>
             
+            <div className="flex justify-center items-start gap-4">
+              <img
+                className="w-8"
+                src="https://cdn-icons-png.flaticon.com/128/10093/10093570.png"
+                alt=""
+              />
+              <p className="text-3xl md:text-start text-center "> Develop seamless and high-performance front-end solutions for web-based applications</p>
+            </div>
+            <div className="flex justify-center items-start gap-4">
+              <img
+                className="w-8"
+                src="https://cdn-icons-png.flaticon.com/128/10093/10093570.png"
+                alt=""
+              />
+              <p className="text-3xl md:text-start text-center ">I can solve your front-end issues efficiently</p>
+            </div>
+            <div className="flex justify-center items-start gap-4">
+              <img
+                className="w-8"
+                src="https://cdn-icons-png.flaticon.com/128/10093/10093570.png"
+                alt=""
+              />
+              <p className="text-3xl md:text-start text-center "> We focus on both UI and UX to ensure a seamless user experience</p>
+            </div>
           </div>
-        <div className="flex  items-center justify-center md:flex-row flex-col">
-        <div className="flex flex-col justify-center items-center md:items-start gap-4">
-          
 
-          <p className="md:w-96 md:text-2xl w-full md:text-start text-center px-16 ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quis
-            magni temporibus recusandae dolorum dolore iusto, vitae aperiam,
-            quaerat aliquam autem incidunt esse nobis enim voluptate, beatae
-            praesentium adipisci reprehenderit. dont forget bootstrap to add it
-          </p>
-        </div>
-
-        <div className="flex justify-center items-center gap-4 flex-col">
-          <div className="flex justify-center items-center gap-8">
-            <img
-              className="md:w-12 w-20"
-              src="https://cdn-icons-png.flaticon.com/128/5968/5968267.png"
-              alt="logo"
-            />
-            <img
-              className="md:w-12 w-20"
-              src="https://cdn-icons-png.flaticon.com/128/5968/5968242.png"
-              alt="logo"
-            />
-            <img
-              className="md:w-12 w-20"
-              src="https://cdn-icons-png.flaticon.com/128/5968/5968292.png"
-              alt="logo"
-            />
-          </div>
-          <div className="flex justify-center items-center gap-8">
-            <img
-              className="md:w-12 w-20"
-              src="https://cdn-icons-png.flaticon.com/128/1126/1126012.png"
-              alt="logo"
-            />
-            <img
-              className="md:w-12 w-20"
-              src="https://imgs.search.brave.com/p5MjVdZG2puGdL4uizeF2dUQHiuwXKEw6-BPZ3zlGJE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi9kL2RiL05w/bS1sb2dvLnN2Zy8y/MjBweC1OcG0tbG9n/by5zdmcucG5n"
-              alt="logo"
-            />
-            <img
-              className="md:w-12 w-20"
-              src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png"
-              alt="logo"
-            />
-          </div>
-          <div className="flex justify-center items-center gap-8">
-            <img
-              className="md:w-12 w-20"
-              src="https://cdn-icons-png.flaticon.com/128/15466/15466163.png"
-              alt="logo"
-            />
-            <img
-              className="md:w-12 w-20"
-              src="https://cdn-icons-png.flaticon.com/128/16021/16021236.png"
-              alt="logo"
-            />
-            <img
-              className="md:w-12 w-20"
-              src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg"
-              alt="logo"
-            />
+          <div className="flex justify-center items-center gap-4 flex-col">
+            <div className="flex justify-center items-center gap-8">
+              <img
+                className="md:w-20 w-20"
+                src="https://cdn-icons-png.flaticon.com/128/5968/5968267.png"
+                alt="logo"
+              />
+              <img
+                className="md:w-20 w-20"
+                src="https://cdn-icons-png.flaticon.com/128/5968/5968242.png"
+                alt="logo"
+              />
+              <img
+                className="md:w-20 w-20"
+                src="https://cdn-icons-png.flaticon.com/128/5968/5968292.png"
+                alt="logo"
+              />
+            </div>
+            <div className="flex justify-center items-center gap-8">
+              <img
+                className="md:w-20 w-20"
+                src="https://cdn-icons-png.flaticon.com/128/1126/1126012.png"
+                alt="logo"
+              />
+              <img
+                className="md:w-20 w-20"
+                src="https://imgs.search.brave.com/p5MjVdZG2puGdL4uizeF2dUQHiuwXKEw6-BPZ3zlGJE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi9kL2RiL05w/bS1sb2dvLnN2Zy8y/MjBweC1OcG0tbG9n/by5zdmcucG5n"
+                alt="logo"
+              />
+              <img
+                className="md:w-20 w-20"
+                src="https://cdn-icons-png.flaticon.com/128/7544/7544562.png"
+                alt="logo"
+              />
+            </div>
+            <div className="flex justify-center items-center gap-8">
+              <img
+                className="md:w-20 w-20"
+                src="https://cdn-icons-png.flaticon.com/128/15466/15466163.png"
+                alt="logo"
+              />
+              <img
+                className="md:w-20 w-20"
+                src="https://cdn-icons-png.flaticon.com/128/16021/16021236.png"
+                alt="logo"
+              />
+              <img
+                className="md:w-20 w-20"
+                src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg"
+                alt="logo"
+              />
+            </div>
           </div>
         </div>
-        </div>
-        
       </section>
     </>
   );
