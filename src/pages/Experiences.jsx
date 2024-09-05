3e775c79746f295062988ff0f47028d34d6aa0b1
Djebril <djebrilacademy@gmail.com>
2024-09-05 13:04:07 +0200

    adding diplomes and certificates

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -1,31 +1,57 @@
 const Experiences = () => {
   return (
     <section id="experience">
-      <h1>Diplomes</h1>
-      <div className="flex justify-center items-center flex-col md:flex-row">
-        <div className="flex flex-col justify-center items-center ">
-          <img src="" alt="" />
-          <h1 className="capitalize md:text-3xl text-2xl">Front end web dev</h1>
-          <p></p>
+      <h1 className="md:text-5xl text-center my-8">Diplomes</h1>
+      <div className="flex justify-center items-center flex-col gap-8">
+      <div className="flex justify-center items-center flex-col md:flex-row gap-4">
+        <div className="flex flex-col justify-center items-center md:w-1/2  gap-4">
+          <img
+            className="w-3/4 border-2 border-black "
+            src="https://i.ibb.co/4ShX8Qq/udacity-the-first-certificate.jpg"
+            alt=""
+          />
+          <h1 className="capitalize md:text-3xl text-2xl">
+            Full stack web development
+          </h1>
+          <p className="uppercase">By Udacity</p>
         </div>
-        <div className="flex flex-col justify-center items-center ">
-          <img src="" alt="" />
-          <h1 className="capitalize md:text-3xl text-2xl">Global marketing code 213</h1>
-          <p></p>
+        <div className="flex flex-col justify-center items-center md:w-1/2  gap-4">
+          <img
+            className="w-3/4 border-2 border-black "
+            src="https://i.ibb.co/17gm4tc/Djebril-Zidour-certificate-C1-1.jpg"
+          />
+          <h1 className="capitalize md:text-3xl text-2xl">
+            English C1.1 level
+          </h1>
+          <p className="uppercase"> by American institute</p>
         </div>
       </div>
-      <div className="flex justify-center items-center flex-col md:flex-row">
-        <div className="flex flex-col justify-center items-center ">
-          <img src="" alt="" />
-          <h1 className="capitalize md:text-3xl text-2xl">Facebook Ads izi Road</h1>
-          <p></p>
+      <div className="flex justify-center items-center flex-col md:flex-row gap-4">
+        <div className="flex flex-col justify-center items-center md:w-1/2  gap-4">
+          <img
+            className="w-3/4 border-2 border-black "
+            src="https://i.ibb.co/n1g1WGD/djebril-zidour-Facebook-Ads-Certificat-de-reussite-Etudz-Academy-copy.jpg"
+            alt=""
+          />
+          <h1 className="capitalize md:text-3xl text-2xl">
+            Complete Facebook Ads Course
+          </h1>
+          <p className="uppercase">by izi road</p>
         </div>
-        <div className="flex flex-col justify-center items-center ">
-          <img src="" alt="" />
-          <h1 className="capitalize md:text-3xl text-2xl">robotic and arduino at Techmology</h1>
-          <p></p>
+        <div className="flex flex-col justify-center items-center md:w-1/2  gap-4">
+          <img
+            className="w-3/4 border-2 border-black "
+            src="https://i.ibb.co/n1g1WGD/djebril-zidour-Facebook-Ads-Certificat-de-reussite-Etudz-Academy-copy.jpg"
+            alt=""
+          />
+          <h1 className="capitalize md:text-3xl text-2xl">
+            robotic and arduino at Techmology
+          </h1>
+          <p className="uppercase">by Techmology</p>
         </div>
       </div>
+      </div>
+     
     </section>
   );
 };
