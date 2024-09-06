bd2b709c80648ee6196051b2ea74c18284ec2282
Djebril <djebrilacademy@gmail.com>
2024-09-07 01:52:23 +0200

    starting the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
+import { Link } from "react-scroll";
 const Footer = () => {
-  <footer>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+  <footer className="flex justify-center items-center flex-col">
+  <h1 className="capitalize text-xl">home</h1>
+  <h1 className="capitalize text-xl">skills</h1>
+  <h1 className="capitalize text-xl">diplome & certificate</h1>
+  <h1 className="capitalize text-xl">contact</h1>
   </footer>;
 };
 export default Footer;
