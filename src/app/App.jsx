7e7be90975254b130f5dfc6f31d1db432608045f
Djebril <djebrilacademy@gmail.com>
2024-09-07 02:11:41 +0200

    well i think that the poltforio is full now

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,7 +5,7 @@ import Experiences from "../pages/Experiences";
 import Contact from "../pages/Contact";
 import Skill from "../pages/Skill";
 import Education from "../pages/Education";
-import Foter from "../components/Foter";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,7 +18,7 @@ function App() {
       <Education />
       <Experiences/>
       <Contact />
-      <Foter />
+
     </>
   );
 }
