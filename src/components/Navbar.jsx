f2c53760046ff74fa31875cfaac782cb05640c7f
Djebril <djebrilacademy@gmail.com>
2024-09-07 02:20:46 +0200

    edit a small error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,51 +18,98 @@ const Navbar = () => {
 
           <div id="1" className=" flex-row text-black-100 md:flex hidden ">
             <li className="px-4 font-semibold text-black cursor-pointer hover:underline text-2xl capitalize">
-            <Link to="home" spy={true} smooth={true} offset={50} duration={500}>
-              Home
+              <Link
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={50}
+                duration={500}
+              >
+                Home
               </Link>
             </li>
             <li className="px-4 font-semibold text-black cursor-pointer hover:underline text-2xl capitalize">
-            <Link to="skill" spy={true} smooth={true} offset={50} duration={500}>
-              skill
+              <Link
+                to="skill"
+                spy={true}
+                smooth={true}
+                offset={50}
+                duration={500}
+              >
+                skill
               </Link>
             </li>
             <li className="px-4 font-semibold text-black cursor-pointer hover:underline text-2xl capitalize">
-            <Link to="experience" spy={true} smooth={true} offset={50} duration={500}>
-              experiences
+              <Link
+                to="experience"
+                spy={true}
+                smooth={true}
+                offset={50}
+                duration={500}
+              >
+                experiences
               </Link>
             </li>
-         
+
             <li className="px-4 font-semibold text-black cursor-pointer hover:underline text-2xl capitalize">
-            <Link to="contact" spy={true} smooth={true} offset={50} duration={500}>
-              contact
+              <Link
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={50}
+                duration={500}
+              >
+                contact
               </Link>
             </li>
           </div>
-          <div  className=" md:hidden flex justify-center items-center">
+          <div className=" md:hidden flex justify-center items-center">
             <li className="px-2 font-semibold text-black cursor-pointer hover:underline text-lg capitalize">
-            <Link to="home" spy={true} smooth={true} offset={50} duration={500}>
-              Home
+              <Link
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={50}
+                duration={500}
+              >
+                Home
               </Link>
             </li>
             <li className="px-2 font-semibold text-black cursor-pointer hover:underline text-lg capitalize">
-            <Link to="skill" spy={true} smooth={true} offset={50} duration={500}>
-              skill
+              <Link
+                to="skill"
+                spy={true}
+                smooth={true}
+                offset={50}
+                duration={500}
+              >
+                skill
               </Link>
             </li>
             <li className="px-2 font-semibold text-black cursor-pointer hover:underline text-lg capitalize">
-            <Link to="experience" spy={true} smooth={true} offset={50} duration={500}>
-              experiences
+              <Link
+                to="experience"
+                spy={true}
+                smooth={true}
+                offset={50}
+                duration={500}
+              >
+                Diplomes
               </Link>
             </li>
-         
+
             <li className="px-2 font-semibold text-black cursor-pointer hover:underline text-lg capitalize">
-            <Link to="contact" spy={true} smooth={true} offset={50} duration={500}>
-              contact
+              <Link
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={50}
+                duration={500}
+              >
+                contact
               </Link>
             </li>
           </div>
-          
         </ul>
       </nav>
     </>
