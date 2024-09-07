322d2c3fe6d23a20f8b8788e559105fea2e7ae24
Djebril <djebrilacademy@gmail.com>
2024-09-07 02:03:26 +0200

    done contact part

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,62 +1,21 @@
 const Contact = () => {
   return (
-    <section id="contact" className="flex justify-center items-center  flex-col gap-16 border-2 m-4">
-      <div>
-        <h1>you can contact me by E-mail or phone number for pro contact </h1>
-        <h2>
-          and if you want to discuss with me in my field you can be comfortable
-          contacting me by my instagram account{" "}
-        </h2>
-      </div>
-
-      <div className="flex justify-center items-center gap-4 w-full ">
-        <div className="border-2 border-black flex justify-center items-center gap-4 flex-col p-4 w-1/2 ml-12 rounded-3xl">
-          <h1>my phone number</h1>
-          <h2>+213 556 94 65 14</h2>
+    <section id="contact" className="flex justify-center items-center  flex-col gap-8  m-4">
+      <h1 className="capitalize text-5xl">Contact</h1>
+      <h1 className="text-3xl capitalize">you can contact me by E-mail or phone number</h1>
+      <div className="flex justify-center items-center flex-col  gap-4 w-full">
+        <div className=" flex justify-center items-center flex-col  gap-4 px-44 py-4 bg-yellow-100 border-2 border-black rounded-2xl ">
+        <h1 className="text-5xl uppercase">my phone number:</h1>
+        <h2 className="text-5xl font-bold">+213 556 94 65 14</h2>
         </div>
-        <div className="border-2 border-black flex justify-center items-center gap-4 flex-col p-4 w-1/2 mr-12 rounded-3xl">
-          <h1>my e-mail</h1>
-          <h2>Djebril.zidour.pro@gmail.com</h2>
+        <div className=" flex justify-center items-center flex-col  gap-4 px-32 py-4 bg-yellow-100 border-2 border-black rounded-2xl ">
+        <h1 className="text-5xl uppercase">my e-mail:</h1>
+        <h2 className="text-5xl font-bold">Djebril.zidour.pro@gmail.com</h2>
         </div>
       </div>
+      
 
-      {/* <div className="flex justify-center items-center gap-16 ">
-        <div className="flex justify-center items-center flex-col gap-8">
-          <h1>check my github over here</h1>
-          <img
-            className="w-24"
-            src="https://cdn-icons-png.flaticon.com/128/3792/3792702.png"
-            alt=""
-          />
-        </div>
-
-        <div className="flex justify-center items-center flex-col gap-8">
-          <h1>check my instagram over here</h1>
-          <img
-            className="w-24"
-            src="https://cdn-icons-png.flaticon.com/128/3792/3792702.png"
-            alt=""
-          />
-        </div>
-        <div className="flex justify-center items-center flex-col gap-8">
-          <h1>check my facebook over here</h1>
-          <img
-            className="w-24"
-            src="https://cdn-icons-png.flaticon.com/128/3792/3792702.png"
-            alt=""
-          />
-        </div>
-        <div className="flex justify-center items-center flex-col gap-8">
-          <h1>check my likendin here</h1>
-          <img
-            className="w-24"
-            src="https://cdn-icons-png.flaticon.com/128/3792/3792702.png"
-            alt=""
-          />
-        </div>
-      </div> */}
-
-      <h1>can't wait to work with you</h1>
+      <h1 className="capitalize text-xl ">can't wait to work with you</h1>
     </section>
   );
 };
